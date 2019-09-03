1f3687e5bd26890c622d71abc258bc16feaba969
RossCr0ss <sobechko.r@gmail.com>
2019-09-03 14:47:34 +0300

    add to tour header interpolations

--- src/components/tour-header-info-section/tour-header-info-section.component.ts
+++ src/components/tour-header-info-section/tour-header-info-section.component.ts
@@ -7,7 +7,28 @@ import { NgStyle } from "@angular/common";
   styleUrls: ["./tour-header-info-section.component.scss"]
 })
 export class TourHeaderInfoSectionComponent implements OnInit {
+  public infoTour = infoTour;
+  public currency = currency;
+  public currentCurrency: string = "UAH";
+
   constructor() {}
 
   ngOnInit() {}
 }
+
+const infoTour = {
+  nameHotel: "Armir Resort (Ex. Kemer Millennium Resort) 5*",
+  city: "Kemer",
+  country: "Turkey",
+  tourId: 769342,
+  status: "relevant",
+  price: 23124,
+  duration: 6,
+  roomType: "Promo"
+};
+
+const currency = {
+  eur: "EUR",
+  uah: "UAH",
+  usd: "USD"
+};
