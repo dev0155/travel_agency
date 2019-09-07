6b4f9e58e5f48caf1abe373783b902dd44b81d00
natikos <natikos2000@gmail.com>
2019-09-07 22:56:46 +0300

    Changed into reactive forms.

--- src/modules/account/account.module.ts
+++ src/modules/account/account.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Route, RouterModule } from '@angular/router';
-import { AccountComponent } from 'src/pages/account/account.component';
-import { ProfileComponent } from 'src/components/profile/profile.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { UserFormComponent } from 'src/components/profile/user-form/user-form.component';
 import { CompanyFormComponent } from 'src/components/profile/company-form/company-form.component';
+import { ProfileComponent } from 'src/pages/profile/profile.component';
 
 const routes: Route[] = [
   {
@@ -14,12 +13,7 @@ const routes: Route[] = [
   },
 ];
 @NgModule({
-  declarations: [
-    AccountComponent,
-    ProfileComponent,
-    UserFormComponent,
-    CompanyFormComponent,
-  ],
+  declarations: [ProfileComponent, UserFormComponent, CompanyFormComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
--- src/pages/profile/profile.component.ts
+++ src/pages/profile/profile.component.ts
@@ -3,10 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss'],
+  styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit() {}
+  constructor() { }
+
+  ngOnInit() {
+  }
+
 }
