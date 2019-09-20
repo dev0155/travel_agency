7cb68eec4cfb5f7752950647a0d3317436484366
Dmytro Roik <Dimon@Dmytros-MacBook-Pro.local>
2019-09-20 10:48:03 +0300

    added jvectormap

--- src/pages/dashboard/dashboard.component.ts
+++ src/pages/dashboard/dashboard.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { IInfoBlock } from 'src/interfaces/custom/info-block.model';
 import { StatisticsService } from 'src/services/statistics.service';
+declare var jQuery: (arg0: any) => { vectorMap: (data: any) => void; };
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, AfterViewInit {
 
   public data: IInfoBlock[] = [];
 
@@ -17,4 +18,30 @@ export class DashboardComponent implements OnInit {
     this.data = this.statisticService.data;
   }
 
+  ngAfterViewInit() {
+    this.renderMap('#world-map');
+  }
+
+  /**
+   * Render map jvectormap
+   * @param selector -- container selector
+   * docs:
+   *    http://jvectormap.com/documentation/
+   *    http://jvectormap.com/documentation/javascript-api/jvm-map/
+   */
+  renderMap(selector: string): void {
+    jQuery(selector).vectorMap({
+      map: 'world_mill_en',
+      backgroundColor: 'white',
+      regionStyle: {
+        initial: {
+          fill: '#e1e4e7',
+          'fill-opacity': 1,
+          stroke: 'none',
+          'stroke-width': 0,
+          'stroke-opacity': 1
+        },
+      }
+    });
+  }
 }
