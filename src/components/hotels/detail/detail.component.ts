815b1bc372a9a42d4cd8c95a757f503695a51ccb
natikos <natikos2000@gmail.com>
2019-10-03 14:37:23 +0300

    Hotel detail page.

---
+++ src/components/hotels/detail/detail.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hotel-detail',
+  templateUrl: './detail.component.html',
+  styleUrls: ['./detail.component.scss'],
+})
+export class HotelDetailComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/modules/hotels/hotels.module.ts
+++ src/modules/hotels/hotels.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
-import { CommonModule as Common } from 'src/modules/common/common.module';
 import { NgSelectModule } from '@ng-select/ng-select';
-import { HotelItemComponent } from 'src/components/hotels/hotel-item/hotel-item.component';
-import { CreatingHotelComponent } from 'src/components/hotels/creating/creating.component';
-import { NewHotelFormComponent } from 'src/components/hotels/creating/new-form/new-form.component';
-import { HotelsComponent } from 'src/pages/hotels/hotels.component';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { EffectsModule } from '@ngrx/effects';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { SimpleNotificationsModule } from 'angular2-notifications';
+
+import { CommonModule as Common } from 'src/modules/common/common.module';
 import { UploadHotelImgComponent } from 'src/components/hotels/creating/upload-img/upload-img.component';
 import { HotelService } from 'src/services/hotel.service';
-import { SimpleNotificationsModule } from 'angular2-notifications';
-import { EffectsModule } from '@ngrx/effects';
 import { HotelEffects } from 'src/store/effects/hotel.effects';
-import { NgxPaginationModule } from 'ngx-pagination';
+import { HotelsComponent } from 'src/pages/hotels/hotels.component';
+import { CreatingHotelComponent } from 'src/components/hotels/creating/creating.component';
+import { NewHotelFormComponent } from 'src/components/hotels/creating/new-form/new-form.component';
+import { HotelItemComponent } from 'src/components/hotels/hotel-item/hotel-item.component';
+import { HotelDetailComponent } from 'src/components/hotels/detail/detail.component';
 
 const routes: Routes = [
   {
@@ -22,6 +24,7 @@ const routes: Routes = [
   },
   {
     path: ':id/detail',
+    component: HotelDetailComponent,
   },
   {
     path: 'creating',
@@ -36,6 +39,7 @@ const routes: Routes = [
     CreatingHotelComponent,
     NewHotelFormComponent,
     UploadHotelImgComponent,
+    HotelDetailComponent,
   ],
   imports: [
     CommonModule,
